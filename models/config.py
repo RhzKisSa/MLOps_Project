54584c42056018fdd0d95b99041aa78776a46fde
khanh <khanh2003dakdoa@gmail.com>
2025-05-30 12:24:34 +0700

    khanh commit

---
+++ models/config.py
@@ -0,0 +1,9 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import GPT4AllEmbeddings
+
+embedding_model = None
+llm_model = None
+PDF_DATA_PATH = None
+VECTOR_DB_PATH = None
